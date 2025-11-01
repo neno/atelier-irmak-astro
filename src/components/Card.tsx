e763fd924970e6c405f6b738b1b15dcac8c3e182
Nenad Brcic <neno@neno.ch>
2025-11-01 13:39:42 +0100

    Add biome, format and lint fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,14 @@
 export const Card = ({
-  title,
-  description,
+	title,
+	description,
 }: {
-  title: string;
-  description: string;
+	title: string;
+	description: string;
 }) => {
-  return (
-    <div className="m-4 bg-white rounded-lg shadow-md p-4 border border-gray-200 shadow-sm">
-      <h3 className="text-lg font-bold">{title}</h3>
-      <p className="text-gray-600">{description}</p>
-    </div>
-  );
+	return (
+		<div className="m-4 bg-white rounded-lg shadow-md p-4 border border-gray-200 shadow-sm">
+			<h3 className="text-lg font-bold">{title}</h3>
+			<p className="text-gray-600">{description}</p>
+		</div>
+	);
 };
